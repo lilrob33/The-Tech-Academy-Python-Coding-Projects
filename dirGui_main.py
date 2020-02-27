57383a14cb455cddb4cf5b2d783db9b446e637e7
lil_rob_33 <48527902+lilrob33@users.noreply.github.com>
2020-02-26 17:07:45 -0800

    Add dirGui_main.py to repo

---
+++ dirGui_main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Python Ver:   3.8.1
+#
+# Author:       Pedro Suarez
+#
+# Purpose:      For this drill, you will need to write a script that
+#               creates a GUI with a button widget and a text widget.
+#               Your script will also include a function that when
+#               it is called will invoke a dialog modal which will
+#               allow users with the ability to select a folder directory
+#               from their system. Finally, your script will show the
+#               user’s selected directory path into the text field.
+#
+# Tested OS:  This code was written and tested to work with Windows 10.
+
+
